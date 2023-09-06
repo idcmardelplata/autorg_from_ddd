4975603239e5e9156875b2c8bea1100984ccdbff
ven <alessandrovmkt@gmail.com>
2023-09-06 11:55:06 -0300

    Clarification flow contains clarification questions

--- autorg/flow/flow.py
+++ autorg/flow/flow.py
@@ -3,11 +3,13 @@ class Flow:
     def __init__(self,name):
         self.name = name
         self.preguntas= list()
-        self.__load_questions(name)
+        self.__load_questions(name) 
 
     def __load_questions(self,name:str):
         if name.__eq__("Clarification"):
-            self.preguntas.append(Pregunta("Que significa esto para mi"))
+            lista_preguntas = ["Que significa esto para mi","Es accionable?","Que creo que debo hacer","Cuantas acciones atomicas","Depende de mi?","Es accion siguiente o esta bloqueada?","El no accionable tiene valor potencial?"]
+            for pregunta in lista_preguntas:
+                self.preguntas.append(Pregunta(pregunta))
         if name.__eq__("Another"):
             self.preguntas.append(Pregunta("Esto tiene sentido?"))
 
--- tests/test_flow.py
+++ tests/test_flow.py
@@ -1,7 +1,9 @@
 from autorg.flow.flow import Flow
+from autorg.pregunta.pregunta import Pregunta
 import pytest
 
 # Kind of flow should be an enum 
+# flow should use strategy for load questions
 def test_given_a_name_flow_should_exists():
     assert Flow("Clarification") != None
 
@@ -9,3 +11,9 @@ def test_flow_questions_should_differ_between_a_clarification_and_another_flow()
     clarifi_questions = Flow("Clarification").get_questions()
     another_questions = Flow("Another").get_questions()
     assert clarifi_questions[0].get_question() != another_questions [0].get_question()
+
+def test_aa_clarification_flow_should_contain_clarification_questions():
+    str_clarification_questions = ["Que significa esto para mi","Es accionable?","Que creo que debo hacer","Cuantas acciones atomicas","Depende de mi?","Es accion siguiente o esta bloqueada?","El no accionable tiene valor potencial?"]
+    clarification_questions = [Pregunta(x) for x in str_clarification_questions]
+    flow_questions = Flow("Clarification").get_questions()
+    assert all([clarification_questions.get_question() == flow_questions.get_question() for clarification_questions, flow_questions in zip(clarification_questions, flow_questions)])
