852c9e8a59eadb80df9d8109d9399e79e130212e
ven <alessandrovmkt@gmail.com>
2023-09-06 11:44:01 -0300

    Flows questions difeer between clarification an another flow

--- autorg/flow/flow.py
+++ autorg/flow/flow.py
@@ -3,4 +3,13 @@ class Flow:
     def __init__(self,name):
         self.name = name
         self.preguntas= list()
-        self.preguntas.append(Pregunta("Que significa esto para mi"))
+        self.__load_questions(name)
+
+    def __load_questions(self,name:str):
+        if name.__eq__("Clarification"):
+            self.preguntas.append(Pregunta("Que significa esto para mi"))
+        if name.__eq__("Another"):
+            self.preguntas.append(Pregunta("Esto tiene sentido?"))
+
+    def get_questions(self):
+        return self.preguntas
--- tests/test_flow.py
+++ tests/test_flow.py
@@ -1,5 +1,11 @@
 from autorg.flow.flow import Flow
 import pytest
 
+# Kind of flow should be an enum 
 def test_given_a_name_flow_should_exists():
-        assert Flow("Clarification") != None
+    assert Flow("Clarification") != None
+
+def test_flow_questions_should_differ_between_a_clarification_and_another_flow():
+    clarifi_questions = Flow("Clarification").get_questions()
+    another_questions = Flow("Another").get_questions()
+    assert clarifi_questions[0].get_question() != another_questions [0].get_question()
