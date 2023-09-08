a8942e0c13e244562f7a20737bde25769b3fb3ce
ven <alessandrovmkt@gmail.com>
2023-09-08 13:12:52 -0300

    Add tipes to clarification service

--- autorg/domain/services/clarification/clarification_service.py
+++ autorg/domain/services/clarification/clarification_service.py
@@ -1,8 +1,9 @@
 from autorg.domain.entities.pregunta.pregunta import Pregunta
-from autorg.domain.services.flow.flow import Flow
+from autorg.domain.services.flow.flow_abs import Flow_ABS
 class Clarificacion_SRV:
 
-    def __init__(self,flow):
+
+    def __init__(self,flow:Flow_ABS):
         self.flow = flow
 
     def get_flow(self):
