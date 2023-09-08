53912223163625a7685f83ef2dcb40f9e7aae2b0
ven <alessandrovmkt@gmail.com>
2023-09-08 13:24:00 -0300

    Add types for significado entity and implement its interface
    Fixes #4
    Refactorice todo el codigo de test y prod relevante para el objetivo de
    la release, manteniendo estructura fisica de ddd.

--- autorg/domain/entities/significado/significado_abs.py
+++ autorg/domain/entities/significado/significado_abs.py
@@ -1,6 +1,4 @@
-from .significado_abs import Significado_ABS
 # Significado es una entidad debido a que sera rastreada mediante el flujo de procesamiento.
-class Significado(Significado_ABS):
+class Significado_ABS:
     def __init__(self,significado:str):
-        super().__init__(significado)
-
+        self.significado = significado
