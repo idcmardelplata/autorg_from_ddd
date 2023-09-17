2bc088ad75c642c67eef443da4a391e413a76dcd
Martin Algañaraz <idcmardelplata@gmail.com>
2023-09-17 11:01:08 -0300

    ci: added step for verify code vulnerability

--- ci/build_stage.py
+++ ci/build_stage.py
@@ -6,16 +6,23 @@ async def build_package():
     config = dagger.Config(log_output=sys.stdout)
 
     async with dagger.Connection(config) as client:
-        build = (
+
+        container = (
             client.container()
             .from_("idcmardelplata/poetry:v01")
             .with_directory("/src", client.host().directory("."), exclude=["ci"])
             .with_workdir("/src")
-            .with_exec(["poetry", "install"])
-            # .with_exec(["ls", "dist"]) # Show the whl and tgz output file
-            .with_exec(["poetry", "run", "autorg"])
-            )
+            .with_exec(["poetry", "install"]))
+
+        verify_code = container.with_exec(["poetry", "run", "bandit", "-r", "autorg"])
+        build = container.with_exec(["poetry", "run", "autorg"])
+
+        # Verify code vulnerabilities
+        await verify_code.stderr()
+
+        # Run the code
         await build.stdout()
+
 
 async def main():
     await build_package()
