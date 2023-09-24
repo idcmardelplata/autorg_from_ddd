2dd872a12d91f8283ab06fb9c606dfe1b634105f
Martin Algañaraz <idcmardelplata@gmail.com>
2023-09-24 18:06:57 -0300

    chore: remove duplication and dead code

--- autorg/domain/entities/pregunta/pregunta_abs.py
+++
@@ -1,7 +0,0 @@
-class Pregunta_ABS:
-
-    def __init__(self,pregunta:str):
-        self.pregunta = pregunta
-
-    def get_question(self):
-        pass
--- tests/helpers/fake_pregunta.py
+++
@@ -1,9 +0,0 @@
-from autorg.domain.entities.pregunta.pregunta_abs  import Pregunta_ABS
-class Fake_Pregunta(Pregunta_ABS):
-
-    def __init__(self,pregunta):
-        self.pregunta = pregunta
-
-    def get_question(self):
-        return self.pregunta
-
