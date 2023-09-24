ad16b5aee3ed1a37ff56b95ca10948fede6db5e4
Martin Algañaraz <idcmardelplata@gmail.com>
2023-09-24 19:06:54 -0300

    chore: clean useless services and entities

--- autorg/domain/entities/significado/significado_abs.py
+++
@@ -1,4 +0,0 @@
-# Significado es una entidad debido a que sera rastreada mediante el flujo de procesamiento.
-class Significado_ABS:
-    def __init__(self,significado:str):
-        self.significado = significado
--- tests/test_significado.py
+++
@@ -1,5 +0,0 @@
-from autorg.domain.entities.significado.significado import Significado
-import pytest
-
-def test_significado_should_be_defined_by_a_string():
-    assert Significado("Esto significa esto") != None
