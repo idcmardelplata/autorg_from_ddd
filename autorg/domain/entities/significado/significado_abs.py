417bd008f7da79ef7d6fbf1b5483d90835ca523f
Martin Algañaraz <idcmardelplata@gmail.com>
2023-09-25 15:06:26 -0300

    chore: add some files for the revert commit

---
+++ autorg/domain/entities/significado/significado_abs.py
@@ -0,0 +1,4 @@
+# Significado es una entidad debido a que sera rastreada mediante el flujo de procesamiento.
+class Significado_ABS:
+    def __init__(self,significado:str):
+        self.significado = significado
